09515a31729ae75aefba786390a874cb2c7dc55d
Jun Yi Hoo <junyihoo1997@gmail.com>
2020-03-05 12:47:24 +0800

    Updates

---
+++ precache-manifest.3f945112284528b87975ed8e4ca74941.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e4cf5baf01ee841de6a4faf110dc48c",
+    "url": "/static-sidejob-dp/index.html"
+  },
+  {
+    "revision": "e6ed1712ae905f141de7",
+    "url": "/static-sidejob-dp/static/css/2.bb9071ef.chunk.css"
+  },
+  {
+    "revision": "814335066dbbba55dbbf",
+    "url": "/static-sidejob-dp/static/css/main.92021992.chunk.css"
+  },
+  {
+    "revision": "e6ed1712ae905f141de7",
+    "url": "/static-sidejob-dp/static/js/2.2966e95e.chunk.js"
+  },
+  {
+    "revision": "814335066dbbba55dbbf",
+    "url": "/static-sidejob-dp/static/js/main.dc66bc51.chunk.js"
+  },
+  {
+    "revision": "b74a1557b04440001b5b",
+    "url": "/static-sidejob-dp/static/js/runtime~main.03de0cf4.js"
+  },
+  {
+    "revision": "42ab3c2de596ea70be3a77461561318b",
+    "url": "/static-sidejob-dp/static/media/Christofle.42ab3c2d.png"
+  },
+  {
+    "revision": "7b11a6913bd925840bc67eb897c48c8d",
+    "url": "/static-sidejob-dp/static/media/DivinePalate.7b11a691.png"
+  },
+  {
+    "revision": "0f6fe8d0b597817622ad4e42232a3ad7",
+    "url": "/static-sidejob-dp/static/media/DivinePalate2.0f6fe8d0.png"
+  },
+  {
+    "revision": "7abffc7826225bf5b25394cb58b52b29",
+    "url": "/static-sidejob-dp/static/media/FirstCorporateEvents.7abffc78.jpg"
+  },
+  {
+    "revision": "467f83d28f1a17f11d09657e73045227",
+    "url": "/static-sidejob-dp/static/media/FirstFixedIMG.467f83d2.jpg"
+  },
+  {
+    "revision": "40a34d9f395792be52fe0ec73d4276f0",
+    "url": "/static-sidejob-dp/static/media/ForthBackgroundIMG.40a34d9f.jpg"
+  },
+  {
+    "revision": "10d3cd144e64cd3e5d82802472ccdd3e",
+    "url": "/static-sidejob-dp/static/media/ForthFixedIMG.10d3cd14.jpg"
+  },
+  {
+    "revision": "4b0bcee66a79e5bdd5763140f00909c9",
+    "url": "/static-sidejob-dp/static/media/ForthPrivateDining.4b0bcee6.png"
+  },
+  {
+    "revision": "c3b554a30ef22293c1a5d4e2913e4723",
+    "url": "/static-sidejob-dp/static/media/MainIMG.c3b554a3.png"
+  },
+  {
+    "revision": "fe791313144d9b223d36f2f4770d0c1c",
+    "url": "/static-sidejob-dp/static/media/SecondBackgroundIMG.fe791313.jpg"
+  },
+  {
+    "revision": "d8a77c5a82ae4fad0a4b0c64145e68ae",
+    "url": "/static-sidejob-dp/static/media/SecondFixedIMG.d8a77c5a.jpg"
+  },
+  {
+    "revision": "cd6f2ad7243d6e189199362e2537c929",
+    "url": "/static-sidejob-dp/static/media/SecondProductLaunch.cd6f2ad7.jpg"
+  },
+  {
+    "revision": "d65b238f259d1fd82032cf589f630085",
+    "url": "/static-sidejob-dp/static/media/ThirdBackgroundIMG.d65b238f.png"
+  },
+  {
+    "revision": "2c652e70e763c7cfe98012de76e967dd",
+    "url": "/static-sidejob-dp/static/media/ThirdFixedIMG.2c652e70.jpg"
+  },
+  {
+    "revision": "6bc207cb737eef772d36bd7cf6b5eafa",
+    "url": "/static-sidejob-dp/static/media/ThirdWeddingService.6bc207cb.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static-sidejob-dp/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static-sidejob-dp/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static-sidejob-dp/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static-sidejob-dp/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static-sidejob-dp/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
